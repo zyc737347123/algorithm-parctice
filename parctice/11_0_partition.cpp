011df05706af22a97e78e3d4e3a6c2bb030056f1
zyc737347123 <zyc737347123@163.com>
2020-03-04 11:02:44 +0800

    update quick sort

--- parctice/11_0_partition.cpp
+++ parctice/11_0_partition.cpp
@@ -18,40 +18,39 @@ void Swap(int *a, int *b)
 
 void printa(int data[], int len)
 {
-    for(int i = 0; i <= len ; i++)
+    for (int i = 0; i <= len; i++)
         std::cout << data[i] << ' ';
     std::cout << std::endl;
 }
 
 int Parttion(int data[], int start, int end)
 {
-    if(data == nullptr || start < 0 || end < 0 || end < start)
+    if (data == nullptr || start < 0 || end < 0 || end < start)
     {
         return -1;
     }
 
+    // index的初值有意义，但不一定是start，可以是[start, end]任意
     int index = start;
+
+    // 不是为了换start到最后，而是为了将分界值放放到最右边，因为比较是从左到右的
     Swap(&data[index], &data[end]);
 
-    printa(data, end);
+    // printa(data, end);
 
     int small = start - 1;
 
-    for(index = start; index < end; index++)
+    for (index = start; index < end; index++)
     {
-        if(data[index] < data[end])
+        if (data[index] < data[end])
         {
             small++;
-            if(small != index)
+            if (small != index)
             {
-                // 此时data[index]比data[end]小, samll 和 index 之间存在距离
-                // 此时data[small - 1]会是最右边一个比data[end]小的数字
-                // 此时data[small]，是最左边一个比data[end]大的数字
-                // 所以需要交换
-                // 总结分区函数两个下标：samll和index，samll保持指向最右边比data[end]小的数字就可以了
+                // 总结分区函数两个下标：samll和index，samll保持指向从start开始最右边比data[end]小的数字就可以了
                 // 当发现data[index]比data[end]小，交换 data[small+1] 和当前 data[index]即可
                 Swap(&data[small], &data[index]);
-                printa(data, end);
+                // printa(data, end);
                 // 进行交换后，现在的data[samll]还是最右边一个比data[end]小的数字
             }
         }
@@ -59,13 +58,35 @@ int Parttion(int data[], int start, int end)
 
     small++;
     Swap(&data[small], &data[end]);
-    printa(data, end);
+    printa(&data[start], end - start);
     return small;
+}
+
+void quickSort(int data[], int start, int end)
+{
+    int position = Parttion(data, start, end);
+    if (position >= 0)
+    {
+        quickSort(data, start, position - 1);
+        quickSort(data, position + 1, end);
+    }
+}
+
+void quickSort2(int data[], int start, int end)
+{
+    if (start < end)
+    {
+        int position = Parttion(data, start, end);
+        quickSort(data, start, position - 1);
+        quickSort(data, position + 1, end);
+    }
 }
 
 int main()
 {
     int array[] = {3, 1, 6, 2, 4, 5};
-    std::cout << Parttion(array, 0, 5) << std::endl;
+    quickSort2(array, 0, 5);
+    std::cout << std::endl;
+    printa(array, 5);
     return 0;
 }
